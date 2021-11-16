71e37d481243860249f08fbb5e373b587c44a17b
Tocka Ayman <tocka.ayman@outlook.com>
2021-11-16 15:57:42 +0200

    Implement Map Based Services closes #6

---
+++ src/main/java/com/learning/petclinic/services/map/PetServiceMap.java
@@ -0,0 +1,34 @@
+package com.learning.petclinic.services.map;
+
+import com.learning.petclinic.model.Pet;
+import com.learning.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
+
+    @Override
+    public Set<Pet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Pet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Pet save(Pet object) {
+        return super.save(object.getId(), object);
+    }
+
+    @Override
+    public Pet findById(Long id) {
+        return super.findById(id);
+    }
+}
