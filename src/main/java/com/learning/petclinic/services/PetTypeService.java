ba8e2e25d3e0727a734a85eed93eb995111d7ea6
Tocka Ayman <tocka.ayman@outlook.com>
2022-01-05 12:51:28 +0200

    Create pet type map service closes #3

---
+++ src/main/java/com/learning/petclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package com.learning.petclinic.services;
+
+import com.learning.petclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long> {
+}
---
+++ src/main/java/com/learning/petclinic/services/map/PetTypeServiceMap.java
@@ -0,0 +1,39 @@
+package com.learning.petclinic.services.map;
+
+import com.learning.petclinic.model.PetType;
+import com.learning.petclinic.services.CrudService;
+import com.learning.petclinic.services.PetTypeService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile("map")
+public class PetTypeServiceMap extends AbstractMapService<PetType, Long> implements CrudService<PetType, Long>, PetTypeService {
+
+    @Override
+    public Set<PetType> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(PetType petType) {
+        super.delete(petType);
+    }
+
+    @Override
+    public PetType save(PetType petType) {
+        return super.save(petType);
+    }
+
+    @Override
+    public PetType findById(Long id) {
+        return super.findById(id);
+    }
+}
